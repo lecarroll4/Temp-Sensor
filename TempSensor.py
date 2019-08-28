c43ce6304aba977210cffae6b351432b85e8e173
lecarroll4 <lecarroll4@gmail.com>
2019-08-28 16:55:00 -0500

    Changed to Fahrenheit

--- TempSensor.py
+++ TempSensor.py
@@ -10,12 +10,12 @@ gpio=17
 # Use read_retry method. This will retry up to 15 times to
 # get a sensor reading (waiting 2 seconds between each retry).
 humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
-
-
+temperature = temperature * 9/5.0 + 32
 
 while True:
     if humidity is not None and temperature is not None:
         print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+        
     else:
         print('Failed to get reading. Try again! Don t give up!')
     time.sleep(30)
