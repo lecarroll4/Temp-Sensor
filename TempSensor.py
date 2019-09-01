1dd8c24e775a640726b6c60baf1b29addb3a7e5f
lecarroll4 <lecarroll4@gmail.com>
2019-09-01 13:32:35 -0500

    Changed while loop

--- TempSensor.py
+++ TempSensor.py
@@ -11,27 +11,21 @@ sensor=Adafruit_DHT.DHT11
  
 # Set GPIO sensor is connected to
 gpio=17
- 
-# Use read_retry method. This will retry up to 15 times to
-# get a sensor reading (waiting 2 seconds between each retry).
-humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
-temperature = temperature * 9/5.0 + 32
 
-while True:
-    if humidity is not None and temperature is not None:
-        print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-    else:
-        print('Failed to get reading. Try again! Dont give up!')
-        GPIO.output(21,GPIO.LOW)
-    
-    if temperature > 75:
+
+while humidity is not None and temperature is not None:
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
+    temperature = temperature * 9/5.0 + 32
+
+    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+
+    if temperature > 50:
         print ("Too hot!")
         GPIO.output(21,GPIO.HIGH)
     else:
         print ("Tempurature Normal")
         GPIO.output(21,GPIO.LOW)
-    
-    if humidity is None and temperature is None:
-        GPIO.output(21,GPIO.LOW)
-        print ("No Data")
-    time.sleep(30)    
+    time.sleep(30)   
+
+print('Failed to get reading. Try again! Dont give up!')
+GPIO.output(21,GPIO.LOW)
